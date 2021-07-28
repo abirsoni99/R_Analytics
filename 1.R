2620a46df1bdc8053dbfb96e38e6717ff4db6c35
abirsoni99 <79138300+abirsoni99@users.noreply.github.com>
2021-07-28 20:49:04 +0530

    11

---
+++ 1.R
@@ -0,0 +1,27 @@
+dir()
+sessionInfo()
+date()
+Sys.time()
+x <- c(1,2,4,5,6,7,8,10)
+#creating function
+sqr <- function(val)
+{
+  val*val
+}
+sqr(5)
+#matrix of order 2*2, c here is like creating array
+print(matrix(c(1,2,3,4),2,2))
+x = 3
+y <- 4 #preferred
+y
+x <- c(1,2,3)
+length(x)
+sqr(x) #myfunction
+x <- c("Abir","Soni")
+x
+# global var: <<- used 
+print(matrix(c(1,2,3,4),2,2))
+x <-matrix(c(1,2,3,4),2,2)
+x
+# purr package
+#we installed tidyverse via tools
